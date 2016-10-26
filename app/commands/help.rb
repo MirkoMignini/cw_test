096333101ce32b825e9c1b266d5e007a86a06484
MirkoMignini <mirko.mignini@gmail.com>
2016-10-26 21:15:09 +0200

    Added params to help

--- app/commands/help.rb
+++ app/commands/help.rb
@@ -7,8 +7,13 @@ module Commands
 
     def process
       console.commands.each do |command|
-        puts "#{command.command_text} - #{command.help_text}"
+        puts "#{command.command_text} #{params_to_string(command.declared_params)} "\
+             "- #{command.help_text}"
       end
+    end
+
+    def params_to_string(str)
+      str.map(&:first).join(' ').upcase
     end
   end
 end
